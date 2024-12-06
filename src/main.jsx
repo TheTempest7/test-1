4cbfa8f2934cd3331b096a88a94d341356e87171
Yegor <galax.yegor.uaz.s4@gmail.com>
2024-12-06 20:18:39 +0300

    fix: styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './01-App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
